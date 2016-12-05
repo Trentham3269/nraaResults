15a1521d498b1afed2e9fe231cb0357e1173880a
scottdalwood <scottdalwood@gmail.com>
2016-12-05 20:22:39 +1100

    Remove .gitignore and move csv file into data folder

--- global.R
+++ global.R
@@ -4,14 +4,9 @@ library(DT)
 library(dplyr)
 library(readr)
 library(stringr)
-library(plotly)
-
-# Set working directory
-wd <- "~/Documents/Repos/shinyApps/nraaResults/"
-setwd(wd)
 
 # Import csv file 
-df <- read_csv("nraaResults.csv", col_types = str_dup("c", 11))
+df <- read_csv("data/nraaResults.csv", col_types = str_dup("c", 11))
 
 # Concatenate name column
 df$Name <- paste0(df$`Preferred Name`, " ", df$`Last Name`)
